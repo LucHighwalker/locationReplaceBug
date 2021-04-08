1031382928c5423ea95ba61191b78aa47b6516c1
LucHighwalker <luc.highwalker@gmail.com>
2021-04-08 10:44:27 -0700

    recreated bug.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'locationReplaceBug';
+
+  constructor(private location: Location) {
+    // If the page's initial url has a hash parameter, the below should remove url with hash from browser history.
+    // Instead it is as if it simply pushes the new state to the history stack.
+    this.location.replaceState('');
+  }
 }
